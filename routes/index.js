cce434034b354ffcb2fdc18d86b16fd3c7080a1a
Mauricio Serna <mauricioserf@gmail.com>
2017-05-19 13:08:51 -0500

    [Add] auth middleware

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,15 @@ const api     = express.Router()
  */
 
 const ProductController = require('../controllers/product')
+const auth = require('../middlewares/auth')
 
 api.get('/product', ProductController.getProducts)
 api.get('/product/:productId', ProductController.getProduct)
 api.post('/product', ProductController.saveProduct)
 api.put('/product/:productId', ProductController.updateProdct)
 api.delete('/product/:productId', ProductController.deleteProduct)
+api.get('/private', auth.isAuth, function (req, res) {
+  res.status(200).send({ message: 'Tienes accesso' })
+})
 
 module.exports = api
