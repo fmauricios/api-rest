97ee03b5c82ea69bce72062c3a53041aad0dc4c1
Mauricio Serna <mauricioserf@gmail.com>
2017-05-20 10:40:55 -0500

    [Add] login and product view. [Pending] Separate backend and frontend

--- app.js
+++ app.js
@@ -6,6 +6,7 @@
 
 const express    = require('express')
 const bodyParser = require('body-parser')
+const hbs        = require('express-handlebars')
 const app = express()
 
 /*
@@ -16,6 +17,24 @@ const api = require('./routes')
 
 app.use(bodyParser.urlencoded({ extendend: false }))
 app.use(bodyParser.json())
+
+/*
+ * Config express-handlebars
+ */
+
+app.engine('.hbs', hbs({
+  defaultLayout: 'default',
+  extname: '.hbs'
+}))
+
+app.set('view engine', '.hbs')
+
 app.use('/api', api)
+app.get('/login', (req, res) => {
+  res.render('login')
+})
+app.get('/', (req, res) => {
+  res.render('product')
+})
 
 module.exports = app
